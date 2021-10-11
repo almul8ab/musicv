9a4af94d7743f547167b283c6d5ab49c59f0bb5c
ssssfssssfssssfs <91757057+ssssfssssfssssfs@users.noreply.github.com>
2021-10-11 21:09:27 +0300

    Update channelstream.py

--- handlers/channelstream.py
+++ handlers/channelstream.py
@@ -1,47 +1,28 @@
 # Copyright (C) 2021 AmortMusic Project
 
-import json
 import os
 from os import path
-from typing import Callable
 
-import aiofiles
-import aiohttp
-import ffmpeg
 import requests
-import wget
-from PIL import Image, ImageDraw, ImageFont
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
-from pyrogram.types import Voice
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtube_search import YoutubeSearch
-from handlers.play import generate_cover
-from handlers.play import cb_admin_check
-from handlers.play import transcode
-from handlers.play import convert_seconds
-from handlers.play import time_to_seconds
-from handlers.play import changeImageSize
-from config import BOT_NAME as bn
-from config import DURATION_LIMIT
-from config import UPDATES_CHANNEL as updateschannel
-from config import que
-from cache.admins import admins as a
-from helpers.errors import DurationLimitError
-from helpers.decorators import errors
-from helpers.admins import get_administrators
-from helpers.channelmusic import get_chat_id
-from helpers.decorators import authorized_users_only
-from helpers.filters import command, other_filters
-from helpers.gets import get_file_name
+
 from callsmusic import callsmusic
 from callsmusic.callsmusic import client as USER
+from callsmusic.queues import queues
+from config import que, DURATION_LIMIT, BOT_USERNAME, UPDATES_CHANNEL as updateschannel
 from converter.converter import convert
 from downloaders import youtube
-from callsmusic.queues import queues
+from handlers.play import cb_admin_check, generate_cover
+from helpers.filters import command, other_filters
+from helpers.admins import get_administrators
+from helpers.decorators import authorized_users_only
+from helpers.errors import DurationLimitError
+from helpers.gets import get_file_name
 
 chat_id = None
-
 
 
 @Client.on_message(filters.command(["channelplaylist","cplaylist"]) & filters.group & ~filters.edited)
